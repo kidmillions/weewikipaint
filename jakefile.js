a099c5185e6d5f9710ff02c4173747a8946098b7
Chris <claymanchris@gmail.com>
2015-05-31 23:51:33 -0700

    static code analysis working

--- jakefile.js
+++ jakefile.js
@@ -1,7 +1,33 @@
+/*global desc, task, jake, fail, complete */
+
+"use strict";
+
 task('default', ['lint']);
+
 
 desc('lint everything');
 task('lint', [], function() {
     var lint = require('./build/lint/lint_runner.js');
-    lint.validateFile('jakefile.js', {}, {});
+    var files = new jake.FileList();
+    files.include('**/*.js');
+    files.exclude('node_modules');
+
+
+    var options = {
+        node: true,
+        bitwise: true,
+        curly: false,
+        eqeqeq: true,
+        forin: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        noempty: true,
+        nonew: true,
+        undef: true,
+        strict: true
+    };
+
+    lint.validateFileList(files.toArray(), options, {});
 });
